5e442d37d2a0d4fb86bccc355806a05158e6ca41
Konstantin.kazakov <kostya.kazakov.30001@mail.ru>
2024-05-09 14:30:25 +0500

    [CreateProject]

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
+Route::get('/token', function () {
+    return response()->json(['token' => csrf_token()]);
+});
+
+require __DIR__.'/auth.php';
